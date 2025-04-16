ee7a5c9010fed0ded3595ee1c09e2ab1b85bd3b9
paulinepasseri <paulinecpasseri@gmail.com>
2025-04-16 10:46:17 +0200

    fixed bug in fetch

--- javascript/cart.js
+++ javascript/cart.js
@@ -1,21 +1,22 @@
-fetch("http://localhost:3000/carts")
+fetch("http://localhost:3000/carts/cart")
   .then((response) => response.json())
   .then((data) => {
-    if ((result = true)) {
-      for (let i = 0; i< data.carts.length; i++){
+    console.log(data)
+    if ((data.result === true)) {
+      for (let i = 0; i< data.cart.length; i++){
         document.querySelector("#cart-container").innerHTML = `
         <div id="text">
-        <p>My cart</p>
+          <p>My cart</p>
         </div>
         <div class="cart">
-        <span class="cart-info">${data.carts[i].arrival}">"${data.carts[i].departure}</span>
-        <span class="cart-info">${data.carts[i].date}</span>
-        <span class="cart-info">${data.carts[i].price}</span>
-        <button class="delete-btn"><i class="fa-solid fa-xmark"></i></button>
+          <span class="cart-info">${data.cart[i].arrival}">"${data.cart[i].departure}</span>
+          <span class="cart-info">${data.cart[i].date}</span>
+          <span class="cart-info">${data.cart[i].price}</span>
+          <button class="delete-btn"><i class="fa-solid fa-xmark"></i></button>
         </div>
         <div id="total-cart">
-        <span class= "total-price">${data.carts[i].price}</span>
-        <span class= "purchase-btn">Purchase</span>
+          <span class= "total-price">${data.cart[i].price}</span>
+          <span class= "purchase-btn">Purchase</span>
         </div>
     `;
        } }
